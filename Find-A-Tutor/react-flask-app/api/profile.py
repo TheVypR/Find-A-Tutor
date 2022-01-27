b772ae9b142b01eaa2ce69f276b857b606d7016a
TheVypR <57635954+TheVypR@users.noreply.github.com>
2022-01-27 10:45:05 -0500

    Profile stores values from DB in Flask

--- Find-A-Tutor/react-flask-app/api/profile.py
+++ Find-A-Tutor/react-flask-app/api/profile.py
@@ -1,9 +1,9 @@
-from threading import local
 from flask import Flask, request
 
 from flask_wtf import FlaskForm
 from flask_wtf import Form
 from wtforms import BooleanField
+#from MySQLdb import escape_string as thwart
 
 #Database stuff
 from flaskext.mysql import MySQL
@@ -32,13 +32,54 @@ mysql.init_app(app)
 
 @app.route('/myProfile/')
 def profile():
-    #form=ProfileForm()
-    
+    tutor_id = 1
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Professors")
-    data = cursor.fetchone()
-    print(data)
     
-    return data  
+    #get the tutor information from the DB
+    #get the name
+    cursor.execute("select name from Tutor where tutor_id = (%s)", (tutor_id))
+    name = cursor.fetchone()
+    print(name)
+    
+    #get the email
+    cursor.execute("select email from Tutor where tutor_id = (%s)", (tutor_id))
+    email = cursor.fetchall()
+    print(email)
+    
+    #get the classes and rates
+    cursor.execute("select class_code, rate from TutorRates where tutor_id = (%s)", (tutor_id))
+    classes_rates = cursor.fetchall()
+    
+    for classCode in classes_rates:
+        print(classCode[0])
+        print(classCode[1])        
+        #do something with the tuple (print it?)
+    
+    #get the times
+    cursor.execute("select * from TutorTimes where tutor_id = (%s)", (tutor_id))
+    times = cursor.fetchall()
+    print(times)
+    
+    #get the login preference
+    cursor.execute("select log_in_as_tutor from Tutor where tutor_id = (%s)", (tutor_id))
+    loginPref = cursor.fetchone()
+    print(loginPref)
+    
+    #get the contactability
+    cursor.execute("select contact_me from Tutor where tutor_id = (%s)", (tutor_id))
+    contactable = cursor.fetchone()
+    print(contactable)
+    
+    #get the payment
+    cursor.execute("select payment_type, payment_info from Tutor where tutor_id = (%s)", (tutor_id))
+    payment = cursor.fetchone()   
+    
+    #split the payment details
+    payment_method = payment[0]  #payment_type
+    payment_details = payment[1] #payment_info
+    print(payment_method)
+    print(payment_details)
+    
+    return {'title': "A Lemon", 'name': payment_method}  
     
