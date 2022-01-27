95161028395d5cdd816c2074634a5cc1f1fd4c5f
TheVypR <57635954+TheVypR@users.noreply.github.com>
2022-01-26 20:30:06 -0500

    Testing SQL

--- Find-A-Tutor/react-flask-app/api/api.py
+++ Find-A-Tutor/react-flask-app/api/api.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 mysql = MySQL()
 
-app.config['MYSQL__DATABASE_HOST'] = 'localhost'
+app.config['MYSQL__DATABASE_HOST'] = '10.18.110.181:22'
 app.config['MYSQL_DATABASE_USER'] = 'trwarner00'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Timothy21!'
 app.config['MYSQL_DATABASE_DB'] = 'findatutor'
@@ -23,4 +23,4 @@ def start():
     
     print(data)
 
-    return "Yay"
+    return data
--- Find-A-Tutor/react-flask-app/api/profile.py
+++ Find-A-Tutor/react-flask-app/api/profile.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 mysql = MySQL()
 
-app.config['MYSQL__DATABASE_HOST'] = 'localhost'
+app.config['MYSQL__DATABASE_HOST'] = '10.18.110.181:22'
 app.config['MYSQL_DATABASE_USER'] = 'trwarner00'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Timothy21!'
 app.config['MYSQL_DATABASE_DB'] = 'findatutor'
@@ -32,5 +32,5 @@ def profile():
     data = cursor.fetchone()
     print(data)
     
-    return "yay"  
+    return data  
     
