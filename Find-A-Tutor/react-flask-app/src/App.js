fb6b54af747beaaa393e04722a58f86223e16d16
TheVypR <57635954+TheVypR@users.noreply.github.com>
2022-01-29 17:40:00 -0500

    Signup

--- Find-A-Tutor/react-flask-app/src/App.js
+++ Find-A-Tutor/react-flask-app/src/App.js
@@ -4,17 +4,49 @@ import './components/App.css';
 import Calendar from './components/calendar'
 
 function App() {
-  const  [initialData, setInitialData] = useState([{}])
-  
-  useEffect(()=> {
-    fetch('/myProfile/').then(
-      response => response.json()
-    ).then(data => setInitialData(data))
-  }, []);
+  const  [name, setName] = useState("")
+  const  [email, setEmail] = useState("")
+//  useEffect(()=> {
+//  fetch('/signup/', {'method':'POST'}).then(
+//      response => response.json()
+//    ).then(data => setContent(data))
+//  }, []);
   return (
     <div className="App">
-      <Calendar />
-    </div>
+		<form>
+			<label htmlFor="name" className="form-label">Name</label>
+			<input 
+			type="text"
+			className="form-control" 
+			placeholder ="Enter name"
+			value={name}
+			onChange={(e)=>setName(e.target.value)}
+			required
+			/>
+			<label htmlFor="email" className="form-label">Email</label>
+			<input 
+			type="text"
+			className="form-control" 
+			placeholder ="Enter email"
+			value={email}
+			onChange={(e)=>setEmail(e.target.value)}
+			required
+			/>
+			<button 
+			type="submit" 
+			value="todo"
+			onClick={async () => {
+			const todo = { name, email };
+			const response = await fetch("/signup/", {
+			method: "POST",
+			headers: {
+			'Content-Type' : 'application/json'
+			},
+			body: JSON.stringify(todo)
+			})}}>Submit
+			</button>
+		</form>
+	</div>
   );
 }
 
