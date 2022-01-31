b01db02ea750b2384ab303041ab9c8003fa2eecd
TheVypR <57635954+TheVypR@users.noreply.github.com>
2022-01-31 18:28:16 -0500

    Profile Linking

--- Find-A-Tutor/react-flask-app/src/App.js
+++ Find-A-Tutor/react-flask-app/src/App.js
@@ -5,13 +5,6 @@ import Calendar from './components/calendar'
 import TutorProfile from "./components/TutorProfile"
 
 function App() {
-  const  [name, setName] = useState("")
-  const  [email, setEmail] = useState("")
-  useEffect(()=> {
-  fetch('/myProfile/', {'method':'GET'}).then(
-      response => response.json()
-    ).then(data => setName(data))
-  }, []);
   return (
     <div className="App">
 		<TutorProfile />
