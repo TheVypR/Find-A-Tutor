f3f202b6fd37a69b01ed9ce125aeace54485ba64
Njbeam <njbeam@gmail.com>
2022-01-31 15:40:03 -0500

    new calendar

---
+++ Find-A-Tutor/react-flask-app/src/components/calendar.js
@@ -0,0 +1,70 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import React, { Component } from "react";
+
+
+
+
+
+const appointments = [
+  {
+    id: 1,
+    tutorName: 'Isaac Apel',
+    studentName: 'Aaron Sickafuse',
+    start: '2022-01-30T10:00:00',
+    end: '2022-01-30T12:00:00',
+  }
+  
+];
+
+function FullCalendarApp() {
+  function updateEvent() {
+
+  }
+  function addEvent(id, tutorName, studentName, startTime, endTime) {
+    var myEvent = {
+      id: id,
+      tutorName : tutorName,
+      studentName : studentName,
+      start : startTime,
+      end : endTime
+    };
+    appointments.push(myEvent)
+  }
+
+  return (
+    <div className="App">
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        headerToolbar={{
+          center: 'dayGridMonth,timeGridWeek,timeGridDay, addAppointmentButton, editAppointmentButton'
+        }}
+        customButtons={{
+          addAppointmentButton: {
+            text: 'Add New Appointment',
+            click: function(){
+              addEvent('2', 'Nathan Beam', 'Tim Warner', '2022-02-02T10:00:00', '2022-02-02T12:00:00');
+            }
+          },
+          editAppointmentButton: {
+            text: 'Edit Appointment',
+            click: function(){
+              updateEvent();
+            }
+          },
+        
+        }}
+        events={appointments}
+        eventColor="green"
+        nowIndicator
+        dateClick={(e) => console.log(e.dateStr)}
+        eventClick={(e) => console.log(e.event.id)}
+      />
+    </div>
+  );
+}
+
+export default FullCalendarApp
