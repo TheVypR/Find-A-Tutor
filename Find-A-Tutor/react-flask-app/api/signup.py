e7c003778a5f79258af2e2212ea65a093e1b53d9
TheVypR <57635954+TheVypR@users.noreply.github.com>
2022-02-02 17:46:06 -0500

    Readded SQL

--- Find-A-Tutor/react-flask-app/api/signup.py
+++ Find-A-Tutor/react-flask-app/api/signup.py
@@ -3,28 +3,27 @@ from flask import Flask, request
 from flask_wtf import FlaskForm
 from flask_wtf import Form
 from wtforms import BooleanField
-#from MySQLdb import escape_string as thwart
 
 #Database stuff
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
-# mysql = MySQL()
+mysql = MySQL()
 
-# locality = 1 # Have locality set to 1 if you want to test on your local machine
-# if (locality == 1):
-    # app.config['MYSQL_DATABASE_HOST'] = '10.18.110.181'
-    # app.config['MYSQL_DATABASE_USER'] = 'test'
-    # app.config['MYSQL_DATABASE_PASSWORD'] = 'C0dePr0j$'
-    # app.config['MYSQL_DATABASE_DB'] = 'findatutor'
-# else:
-    # app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-    # app.config['MYSQL_DATABASE_USER'] = 'trwarner00'
-    # app.config['MYSQL_DATABASE_PASSWORD'] = 'Timothy21!'
-    # app.config['MYSQL_DATABASE_DB'] = 'findatutor'
+locality = 0 # have locality set to 1 if you want to test on your local machine
+if (locality):
+    app.config['MYSQL_DATABASE_HOST'] = '10.18.110.181'
+    app.config['MYSQL_DATABASE_USER'] = 'test'
+    app.config['MYSQL_DATABASE_PASSWORD'] = 'C0dePr0j$'
+    app.config['MYSQL_DATABASE_DB'] = 'findatutor'
+else:
+    app.config['mysql_database_host'] = 'localhost'
+    app.config['mysql_database_user'] = 'trwarner00'
+    app.config['mysql_database_password'] = 'timothy21!'
+    app.config['mysql_database_db'] = 'findatutor'
 
-# mysql.init_app(app)
+mysql.init_app(app)
 #end database stuff
 
 #class ProfileForm(FlaskForm):
@@ -32,17 +31,18 @@ app = Flask(__name__)
 
 @app.route('/signup/', methods=['POST'])
 def profile():
-    #conn = mysql.connect()
-    #conn.autocommit(True)
-    #cursor = conn.cursor()
+    conn = mysql.connect()
+    conn.autocommit(True)
+    cursor = conn.cursor()
     info = request.get_json()    
     print(info)
-    print(info['firstName'])
-    print(info['email'])
-    print(info['password'])
-    #cursor.execute("insert into Student(student_id, email, name) values (1, \"" + info['email'] + "\", \"" + info['name'] +"\")")
+    print(info[0])
+    print(info[1])
+    print(info[2])
+    print(info[3])
+    cursor.execute("insert into Student(student_id, email, name) values (1, \"" + info['email'] + "\", \"" + info['name'] +"\")")
     
-    #conn.close()
+    conn.close()
 
     return 'Done'
 
