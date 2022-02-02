66163a29d31edf603f502feabd1485145f09bb6d
comp452 <comp452@findatutor.GCC.edu>
2022-02-02 18:02:52 -0500

    Format updates

--- Find-A-Tutor/react-flask-app/api/signup.py
+++ Find-A-Tutor/react-flask-app/api/signup.py
@@ -12,16 +12,16 @@ app = Flask(__name__)
 mysql = MySQL()
 
 locality = 0 # have locality set to 1 if you want to test on your local machine
-if (locality):
+if (locality == 1):
     app.config['MYSQL_DATABASE_HOST'] = '10.18.110.181'
     app.config['MYSQL_DATABASE_USER'] = 'test'
     app.config['MYSQL_DATABASE_PASSWORD'] = 'C0dePr0j$'
     app.config['MYSQL_DATABASE_DB'] = 'findatutor'
 else:
-    app.config['mysql_database_host'] = 'localhost'
-    app.config['mysql_database_user'] = 'trwarner00'
-    app.config['mysql_database_password'] = 'timothy21!'
-    app.config['mysql_database_db'] = 'findatutor'
+  app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+  app.config['MYSQL_DATABASE_USER'] = 'trwarner00'
+  app.config['MYSQL_DATABASE_PASSWORD'] = 'Timothy21!'
+  app.config['MYSQL_DATABASE_DB'] = 'findatutor'
 
 mysql.init_app(app)
 #end database stuff
@@ -40,7 +40,7 @@ def profile():
     print(info[1])
     print(info[2])
     print(info[3])
-    cursor.execute("insert into Student(student_id, email, name) values (1, \"" + info['email'] + "\", \"" + info['name'] +"\")")
+    cursor.execute("insert into Student(student_id, email, name, password) values (8576489, \"" + info[2] + "\", \"" + info[0] + " " + info[1] +"\", \"" + info[3] + "\")")
     
     conn.close()
 
