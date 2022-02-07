981c03d0a12d0da68c3eeebaf561196b3c5bbdba
TheVypR <57635954+TheVypR@users.noreply.github.com>
2022-02-07 18:35:54 -0500

    Uncomment

--- Find-A-Tutor/react-flask-app/api/profile.py
+++ Find-A-Tutor/react-flask-app/api/profile.py
@@ -29,54 +29,54 @@ app = Flask(__name__)
 
 @app.route('/myProfile/', methods=['GET'])
 def retrieve_profile(tut_email):
-    #tut_email = "apelia18@gcc.edu"
-    # conn = mysql.connect()
-    # cursor = conn.cursor()
+    tut_email = "apelia18@gcc.edu"
+    conn = mysql.connect()
+    cursor = conn.cursor()
     
-    # #get the tutor information from the DB
-    # #get the name
-    # cursor.execute("select tut_name from Tutor where tut_email = (%s)", (tut_email))
-    # name = cursor.fetchone()
-    # print(name)
+    #get the tutor information from the DB
+    #get the name
+    cursor.execute("select tut_name from Tutor where tut_email = (%s)", (tut_email))
+    name = cursor.fetchone()
+    print(name)
     
-    # #get the email
-    # cursor.execute("select tut_email from Tutor where tut_email = (%s)", (tut_email))
-    # email = cursor.fetchone()
-    # print(email)
+    #get the email
+    cursor.execute("select tut_email from Tutor where tut_email = (%s)", (tut_email))
+    email = cursor.fetchone()
+    print(email)
     
-    # #get the classes and rates
-    # #cursor.execute("select class_code, rate from TutorClasses where tut_email = (%s)", (tut_email))
-    # #classes_rates = cursor.fetchall()
+    #get the classes and rates
+    cursor.execute("select class_code, rate from TutorClasses where tut_email = (%s)", (tut_email))
+    classes_rates = cursor.fetchall()
     
-    # #for classCode in classes_rates:
-    # #    print(classCode[0])
-    # #    print(classCode[1])        
-        # # #do something with the tuple (print it?)
+    for classCode in classes_rates:
+       print(classCode[0])
+       print(classCode[1])        
+       #do something with the tuple (print it?)
     
-    # #get the times
-    # #cursor.execute("select start_date, end_date from TutorTimes where tut_email = (%s)", (tut_email))
-    # #times = cursor.fetchall()
-    # #print(times)
+    #get the times
+    cursor.execute("select start_date, end_date from TutorTimes where tut_email = (%s)", (tut_email))
+    times = cursor.fetchall()
+    print(times)
     
-    # #get the login preference
-    # cursor.execute("select login_pref from Tutor where tut_email = (%s)", (tut_email))
-    # loginPref = cursor.fetchone()
-    # print(loginPref)
+    #get the login preference
+    cursor.execute("select login_pref from Tutor where tut_email = (%s)", (tut_email))
+    loginPref = cursor.fetchone()
+    print(loginPref)
     
-    # #get the contactability
-    # cursor.execute("select contactable from Tutor where tut_email = (%s)", (tut_email))
-    # contactable = cursor.fetchone()
-    # print(contactable)
+    #get the contactability
+    cursor.execute("select contactable from Tutor where tut_email = (%s)", (tut_email))
+    contactable = cursor.fetchone()
+    print(contactable)
     
-    # #get the payment
-    # cursor.execute("select pay_type, pay_info from Tutor where tut_email = (%s)", (tut_email))
-    # payment = cursor.fetchone()   
+    #get the payment
+    cursor.execute("select pay_type, pay_info from Tutor where tut_email = (%s)", (tut_email))
+    payment = cursor.fetchone()   
     
-    # #split the payment details
-    # payment_method = payment[0]  #payment_type
-    # payment_details = payment[1] #payment_info
-    # print(payment_method)
-    # print(payment_details)
+    #split the payment details
+    payment_method = payment[0]  #payment_type
+    payment_details = payment[1] #payment_info
+    print(payment_method)
+    print(payment_details)
         
     return {'name': "Tim Warner", 'email': "trwarner18@gcc.edu"}  
     
