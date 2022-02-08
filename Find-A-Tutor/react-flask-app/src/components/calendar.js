459d5ec3ea5487a52275b8d46164b596be1de1e7
Njbeam <njbeam@gmail.com>
2022-02-07 19:19:15 -0500

    calendar updates

--- Find-A-Tutor/react-flask-app/src/components/calendar.js
+++ Find-A-Tutor/react-flask-app/src/components/calendar.js
@@ -2,66 +2,77 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-//import React, { Component } from "react";
 
 
+//list of appointments to add to calendar
+//TODO: dynamically load appointments into list via database
 const appointments = [
   {
     id: 1,
     title: 'Isaac Apel',
-    tutorName: 'Isaac Apel',
-    studentName: 'Aaron Sickafuse',
-    start: '2022-01-30T10:00:00',
-    end: '2022-01-30T12:00:00',
+    start: '2022-02-10T16:00:00',
+    end: '2022-02-10T18:00:00',
   },
-  
 ];
 
+
 function FullCalendarApp() {
-  function updateEvent() {
-
-  }
-  function addEvent(id, tutorName, studentName, startTime, endTime) {
-    var myEvent = {
-      id: id,
-      tutorName : tutorName,
-      studentName : studentName,
-      start : startTime,
-      end : endTime
-    };
-    appointments.push(myEvent)
-  }
-
   return (
     <div className="App">
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        initialView="timeGridWeek"
+
+        //user defaults to week view
+        initialView="dayGridMonth"
+
+        /* 
+          set up tab bar at top of calendar
+          current allignment: switch to month view - switch to week view - 
+          switch to day view - add appointment button
+        */
         headerToolbar={{
-          center: 'dayGridMonth,timeGridWeek,timeGridDay, addAppointmentButton, editAppointmentButton',
+          center: 'dayGridMonth,timeGridWeek,timeGridDay, new',
         }}
+
+        //create buttons
+        //TODO: decide if any buttons at top of screen are necessary
         customButtons={{
-          addAppointmentButton: {
-            text: 'Add New Appointment',
-            click: () => console.log('new event'),
-              //addEvent('2', 'Nathan Beam', 'Tim Warner', '2022-02-02T10:00:00', '2022-02-02T12:00:00');
-            },
-          
-          editAppointmentButton: {
-            text: 'Edit Appointment',
-            click: () => console.log('edit event'),
-            //updateEvent();
+          new: {
+
+            //set text for button
+            text: 'create appointment',
+
+            //define function for on click
+            click: () => appointments.push(
+              {
+                id: 2,
+                title: 'test',
+                start: '2022-01-27T10:00:00',
+                end: '2022-02-27T12:00:00',
+            }),
+
           },
-        
-        }}
+
+        }}//end button setup
+
+        //add appointments to calendar
         events={appointments}
+
+        //formatting of appointments
         eventColor="green"
         nowIndicator
-        dateClick={(e) => console.log(e.dateStr)}
-        eventClick={(e) => console.log(e.event.id)}
+
+        //ability to click dates
+        dateClick={(e) => alert(e.dateStr)}
+
+        //ability to click appointments
+        //TODO: add ability to open up more information about appointment via click
+        //TODO: add ability to sign up for appointment via click/on loaded modal
+        eventClick={(e) => 
+          alert('Appointment With: ' + e.event.title)}
       />
     </div>
   );
 }
 
-export default FullCalendarApp
+export default FullCalendarApp;
---
+++ Find-A-Tutor/react-flask-app/src/loser.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+    char *p = NULL;
+{
+    char c;
+    p = &c;
+}
+return 0;
+}
