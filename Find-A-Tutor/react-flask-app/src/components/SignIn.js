82d0ec60380b5104e6bf76b7f0b74f7b9da42705
SlickSick <sickafusea@gmail.com>
2022-02-22 09:29:35 -0500

    finalize authentication

--- Find-A-Tutor/react-flask-app/src/components/SignIn.js
+++ Find-A-Tutor/react-flask-app/src/components/SignIn.js
@@ -35,21 +35,20 @@ export default function SignIn() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-	const info = [data.get('email'), data.get('password')]
-	fetch("/login/", {
-		method: 'POST',
-		headers: {
-		'Content-Type' : 'application/json'
-		},
-		body:JSON.stringify(info)
-	})
-  .then(navigate('/calendar'))
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-	console.log("SignedIn");
+    const info = [data.get('email'), data.get('password')]
+    fetch("/login/", {
+      method: 'POST',
+      headers: {'Content-Type' : 'application/json'},
+      body:JSON.stringify(info)
+    }).then(resp => resp.json())
+    .then(result => {
+      if (result['email'] === info[0]) {
+        loginHandler()
+      }
+      else {
+        logoutHandler()
+      }
+    })
   };
   return (
     <ThemeProvider theme={theme}>
