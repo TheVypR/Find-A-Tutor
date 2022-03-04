9e99f84410dcd6cdcc1f97aa07fb7b01848b1cbc
TheVypR <57635954+TheVypR@users.noreply.github.com>
2022-03-04 09:43:32 -0500

    Reporting SQL

--- Find-A-Tutor/react-flask-app/api/history.py
+++ Find-A-Tutor/react-flask-app/api/history.py
@@ -69,3 +69,14 @@ def submitRating(data):
     newRate = int((rating + data['rating']) / 2)
     cursor.execute("update Tutor set rating = " + newRate + " where tut_email = \"" + data['tutor'] + "\"")
     return "Done"
+    
+def submitStudentReport(data, email):
+    conn = mysql.connect()
+    conn.autocommit(True)
+    cursor = conn.cursor()
+    cursor.execute("insert into ReportedStudents(stu_email, reported_by, reason, report) values (\"" 
+                        + data['student'] + "\", \""
+                        + email + "\", \""
+                        + data['reason'] + "\", \""
+                        + data['report'] + "\")")
+    return "DONE"
