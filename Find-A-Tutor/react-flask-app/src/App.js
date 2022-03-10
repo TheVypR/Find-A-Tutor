398c97bf94e5bf3346122fdc5a0163ff1d221fa6
SlickSick <sickafusea@gmail.com>
2022-03-09 19:21:21 -0500

    add route

--- Find-A-Tutor/react-flask-app/src/App.js
+++ Find-A-Tutor/react-flask-app/src/App.js
@@ -14,6 +14,7 @@ import StudentHistory from './components/StudentHistory';
 import Reports from './components/Reports';
 import CurrentAndBan from './components/CurrentAndBan';
 import LoadProfile from './components/LoadProfile';
+import ContactMe from './components/ContactMe';
 
 import { AuthContext } from './components/AuthContext'
 
@@ -45,6 +46,7 @@ export default function App() {
             <Route path='/StudentHistory' element={<StudentHistory />} />
             <Route path='/Reports' element={<Reports />} />
             <Route path='/CurrentAndBan' element={<CurrentAndBan />} />
+            <Route path='/ContactMe' element={<ContactMe />} />
           </Routes>
         </BrowserRouter>
       </AuthContext.Provider>
