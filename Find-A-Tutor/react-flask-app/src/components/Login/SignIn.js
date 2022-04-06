9763e5a200f6a23c9a9b64a6feef80236763b9ed
Tim Warner <44846153+trwarner00@users.noreply.github.com>
2022-04-06 13:13:54 -0400

    SignIn styling

--- Find-A-Tutor/react-flask-app/src/components/Login/SignIn.js
+++ Find-A-Tutor/react-flask-app/src/components/Login/SignIn.js
@@ -18,6 +18,9 @@ import { AuthContext } from '../AuthContext';
 import GroupTutoring from './GroupTutoring'
 import './LoginCSS.css'
 import AppBar from '@mui/material/AppBar';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import Logo from '../../images/logo.png'
 
 
 const theme = createTheme();
@@ -84,69 +87,80 @@ export default function SignIn(props) {
   return (
     <>
       <AppBar postion="static" color="primary" sx={{ borderTheme: (theme) => `1px solid ${theme.palette.divider}` }}>
-        <p> test </p>
-      </AppBar>
-
-      <div className="Login">
-        <Container component="main" maxWidth="xs">
-          <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <div className="d-flex justify-content-around align-items-center">
+          <div className="d-flex align-items-center">
+            <h1> Welcome to <br /> Find-A-Tutor</h1>
+            <img
+              src={`${Logo}?w=50&h=50&fit=crop&auto=format`}
+              srcSet={`${Logo}?w=50&h=50&fit=crop&auto=format&dpr=2 15x`}
+              alt="Find-A-Tutor Logo"
+              loading="lazy"
+              className='logo'
+            />
+          </div>
+          <div className="d-flex justify-content-center Login">
+            {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
               <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
+            </Avatar> */}
             {wrongLogin ? <WrongSignIn /> : null}
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <div className="d-flex flex-column align-items-center">
               <TextField
                 margin="normal"
+                size="small"
                 required
-                fullWidth
+                style={{ width: '225px', margin: "25px" }}
                 id="email"
                 label="Email Address"
                 name="email"
                 autoComplete="email"
                 autoFocus
+                variant="filled"
+                className='username'
+                InputProps={{
+                  disableUnderline: true, // <== added this
+                }}
               />
+              <Button
+                type="submit"
+                style={{ width: '100px', marginTop: "5px", marginLeft: "5px", marginRight: "5px", display: "inline-block" }}
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}>
+                Sign In
+              </Button>
+            </div>
+
+            <div className="d-flex flex-column">
               <TextField
                 margin="normal"
+                size="small"
                 required
-                fullWidth
+                style={{ width: '225px', margin: "25px" }}
                 name="password"
                 label="Password"
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                className="password"
+                variant="filled"
+                InputProps={{
+                  disableUnderline: true, // <== added this
+                }}
               />
-              {/* <FormControlLabel
+              <Link to='/signup' variant="body2" style={{ display: "inline-block" }} >
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </div>
+            {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
             /> */}
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}>
-                Sign In
-              </Button>
+          </div>
+        </div>
+      </AppBar>
 
-              <Link to='/signup' variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Box>
-          </Box>
-          {/*<Copyright sx={{ mt: 8, mb: 4 }} />*/}
-        </Container>
+      <div className="groupTutoring">
+        <GroupTutoring />
       </div>
-
-      <GroupTutoring />
     </>
   );
 }
