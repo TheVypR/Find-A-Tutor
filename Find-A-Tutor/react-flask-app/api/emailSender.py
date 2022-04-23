9a9234562330755d04734199bcf107614974d650
Njbeam <njbeam@gmail.com>
2022-04-23 17:58:41 -0400

    email gives approve and deny link

--- Find-A-Tutor/react-flask-app/api/emailSender.py
+++ Find-A-Tutor/react-flask-app/api/emailSender.py
@@ -10,12 +10,14 @@ requests = verifyRequestRetrieval()[0]
 
 #go through and send email for each request
 for request in requests['requests']:
-    verify_link = "http://10.18.110.181:3000/approveOrDeny"
+    verify_link = "http://10.18.110.181:3000/approveOrDeny?approve_code={}&approve=1".format(request['approve_code'])
+    deny_link = "http://10.18.110.181:3000/approveOrDeny?approve_code={}&approve=0".format(request['approve_code'])
+    print(verify_link)
     message = Mail(
         from_email='fnadatutor@gmail.com',
-        to_emails=request['prof_email'],
+        to_emails='njbeam@gmail.com',
         subject='Find-A-Tutor Verification Request for {}'.format(request['tut_email']),
-        html_content = '<a href="{}">Click here to verify</a>'.format(verify_link))
+        html_content = '<a href=%s>Click here to accept verification</a> <a href=%s>Click here to deny verification</a>' % (verify_link, deny_link))
         #html_content='Hello, a student {request['tut_name']}, has requested your verification to tutor for {request['class_code']}, please navigate to the link below to accept this verification <a>{verifyLink}</a>')
     try:
         sg=SendGridAPIClient(key)
